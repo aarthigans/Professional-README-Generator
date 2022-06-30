ce01799b1fbe957891f7b60315240319773ed77c
Lakshmi <mlakshmi.76@gmail.com>
2022-06-30 18:42:31 -0400

    adding the functions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,44 +1,74 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'no license') {
+    return `
+  ![badge](https://img.shields.io/badge/license-${license}-blue)
+    `;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'no license') {
+    return `
+    [${license}](https://choosealicense.com/licenses/${license})
+      `;
+    } else {
+      return ' ';
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'no license') {
+    return `
+    ## [License](#table-of-contents)
+    The application is covered under the following license:
+    ${renderLicenseLink(license)}
+      `;
+    } else {
+      return ' ';
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   // return `# ${data.title}
   return `
   # ${data.title}
+
+  ${renderLicenseBadge(data.license)}
+
   ## Table of Contents
+  
   * [Description](#description)
   * [Installation](#installation)
   * [Usage](#usage)
-  * [License](#license)
+  ${renderLicenseTOC(data.license)}
   * [Contributing](#contributing)
   * [Tests](#tests)
   * [Questions](#questions)
 
   
-  ## Description
+  ## [Description](#table-of-contents)
   ${data.description}
-  ## Installation
+  ## [Installation](#table-of-contents)
   ${data.installation}
-  ## Usage
+  ## [Usage](#table-of-contents)
   ${data.usage}
-  ## License
-  ${data.license}
-  ## Contributing
-  ${data.contribute}
-  ## Tests
+  ${renderLicenseSection(data.license)}
+  ## [Contributing](#table-of-contents)
+  ${renderContributingSection(data.confirmContributers, data.contribute)}
+ 
+  ## [Tests](#table-of-contents)
   ${data.test}
-  ## Questions
-  For questions please contact me using the following links:
+  ## [Questions](#table-of-contents)
+  Please contact me using the following links:
   [GitHub](https://github.com/${data.githubUsername})
   [Email: ${data.email}](mailto:${data.email})
 `;
